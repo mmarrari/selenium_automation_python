75d7496751755646e12f4733737d10b4294854ec
Martín Marrari <martinmarrari@gmail.com>
2022-05-29 20:51:48 -0300

    Adding pytest-docker-compose

--- test_prueba_pagina3.py
+++ test_prueba_pagina3.py
@@ -6,10 +6,30 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
+import pytest
+import requests
+from urllib.parse import urljoin
+from urllib3.util.retry import Retry
+from requests.adapters import HTTPAdapter
 
-pytest_plugins = ['docker_compose']
+pytest_plugins = ["docker_compose"]
 
-def test_prueba_pagina3():
+# Invoking this fixture: 'function_scoped_container_getter' starts all services
+@pytest.fixture(scope="function")
+def wait_for_api(function_scoped_container_getter):
+    """Wait for the api from my_api_service to become responsive"""
+    request_session = requests.Session()
+    retries = Retry(total=5,
+                    backoff_factor=0.1,
+                    status_forcelist=[500, 502, 503, 504])
+    request_session.mount('http://', HTTPAdapter(max_retries=retries))
+
+    service = function_scoped_container_getter.get("web").network_info[0]
+    api_url = "http://%s:%s/" % (service.hostname, service.host_port)
+    assert request_session.get(api_url)
+    return request_session, api_url
+
+def test_prueba_pagina3(wait_for_api):
     options = Options()
     options.add_argument("start-maximized")
 
