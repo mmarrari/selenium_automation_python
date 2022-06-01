313d5c54439d2a9210979889342ceff142ed06bf
Martín Marrari <martinmarrari@gmail.com>
2022-06-01 14:43:30 -0300

    Refactor code

--- demo_automation.py
+++ demo_automation.py
@@ -8,22 +8,43 @@ from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 
 
-options = Options()
-options.add_argument("start-maximized")
+def configurate_driver():
+    global driver
+    options = Options()
+    options.add_argument("start-maximized")
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    # browser = webdriver.Chrome(executable_path=r"/home/mmarrari/demo_browser_automation/chromedriver")
+    # browser.maximize_window()
+    driver.get('http://localhost:8080/pagina1.html')
+    time.sleep(3)
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-# browser = webdriver.Chrome(executable_path=r"/home/mmarrari/demo_browser_automation/chromedriver")
-# browser.maximize_window()
-driver.get('http://localhost:8080/pagina1.html')
-time.sleep(3)
-wait = WebDriverWait(driver, 20)
-wait.until(EC.element_to_be_clickable((By.ID, 'link1')))
-link_to_press = driver.find_element(By.ID, 'link1')
-link_to_press.click()
-time.sleep(3)
-wait.until(EC.element_to_be_clickable((By.XPATH, '//a[1]')))
-link_to_press = driver.find_element(By.XPATH, '//a[1]')
-link_to_press.click()
-time.sleep(3)
-driver.close()
-driver.quit()
+
+def navigate_to_page2():
+    global wait, link_to_press
+    wait = WebDriverWait(driver, 20)
+    wait.until(EC.element_to_be_clickable((By.ID, 'link1')))
+    link_to_press = driver.find_element(By.ID, 'link1')
+    link_to_press.click()
+    time.sleep(3)
+
+
+def navigate_to_page3():
+    global link_to_press
+    wait.until(EC.element_to_be_clickable((By.XPATH, '//a[1]')))
+    link_to_press = driver.find_element(By.XPATH, '//a[1]')
+    link_to_press.click()
+    time.sleep(3)
+
+
+def close_and_quit_browser():
+    driver.close()
+    driver.quit()
+
+
+configurate_driver()
+
+navigate_to_page2()
+
+navigate_to_page3()
+
+close_and_quit_browser()
